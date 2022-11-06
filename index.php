e1fc7ced8e56b590e53f2a0b629a209fdbf9fba8
Borjampm <bmarquezdelaplata@uc.cl>
2022-11-06 16:58:46 -0300

    consulta 2 v2?

--- index.php
+++ index.php
@@ -9,21 +9,44 @@
   <h3 align="center"> ¿Quieres ver los nombres artisticos de todos los artistas y sus numeros de contacto?</h3>
 
   <form align="center" action="consultas/1.php" method="post">
-    <input type="submit" value="Buscar">
+    <input type="submit" value="VER">
   </form>
 
   <br>
   <br>
   <br>
 
-  <h3 align="center"> ¿Quieres ver las entradas de cortesia que ha entregado un artista?</h3>
+  <h3 align="center"> ¿Quieres ver la cantidad de entradas de cortesia que ha entregado un artista?</h3>
 
   <form align="center" action="consultas/2.php" method="post">
-    Nombre Artistico:
+    Escriba un Nombre Artistico:
     <input type="text" name="nombre">
     <br/><br/>
     <input type="submit" value="Buscar">
   </form>
+
+  <?php require("config/conexion.php");
+  $result = $db -> prepare("SELECT DISTINCT nombre_artistico FROM artistas;");
+  $result -> execute();
+  $dataCollected = $result -> fetchAll();
+  ?>
+
+  <form align="center" action="consultas/2.php" method="post">
+    O seleccinar uno:
+    <select name="nombre">
+      <?php
+      #Para cada tipo agregamos el tag <option value=value_of_param> visible_value </option>
+      foreach ($dataCollected as $d) {
+        echo "<option value=$d[0]>$d[0]</option>";
+      }
+      ?>
+    </select>
+    <br><br>
+    <input type="submit" value="Buscar">
+  </form>
+
+
+
 
   <br>
   <br>
@@ -44,7 +67,7 @@
 <h3 align="center"> ¿Quieres ver los paises a visitar en un tour?</h3>
 
 <form align="center" action="consultas/4.php" method="post">
-  Nombre Artistico:
+  Escriba un Tour:
   <input type="text" name="nombre">
   <br/><br/>
   <input type="submit" value="Buscar">
@@ -56,7 +79,7 @@
 <h3 align="center"> ¿Quieres ver la las productoras con las que ha trabajado un artista?</h3>
 
 <form align="center" action="consultas/5.php" method="post">
-  Nombre Artistico:
+  Escriba un Nombre Artistico:
   <input type="text" name="nombre">
   <br/><br/>
   <input type="submit" value="Buscar">
@@ -68,7 +91,7 @@
 <h3 align="center"> ¿Quieres ver en cuales hoteles se ha hospedado un artista, y cuantas veces?</h3>
 
 <form align="center" action="consultas/6.php" method="post">
-  Nombre Artistico:
+  Escriba un Nombre Artistico:
   <input type="text" name="nombre">
   <br/><br/>
   <input type="submit" value="Buscar">
@@ -80,10 +103,7 @@
 <h3 align="center"> ¿Quieres ver al artista que ha entregado la mayor cantidad de entradas de cortesia?</h3>
 
 <form align="center" action="consultas/7.php" method="post">
-  Nombre Artistico:
-  <input type="text" name="nombre">
-  <br/><br/>
-  <input type="submit" value="Buscar">
+  <input type="submit" value="Ver">
 </form>
 
   <br>

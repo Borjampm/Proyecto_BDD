1be12bcbc54627e1abda21684af0de415f711218
Borjampm <bmarquezdelaplata@uc.cl>
2022-11-06 17:09:07 -0300

    gew

--- index.php
+++ index.php
@@ -92,6 +92,26 @@
   <br/><br/>
   <input type="submit" value="Buscar">
 </form>
+<br/><br/>
+  <?php require("config/conexion.php");
+  $result = $db -> prepare("SELECT DISTINCT nombre FROM tours;");
+  $result -> execute();
+  $dataCollected = $result -> fetchAll();
+  ?>
+
+  <form align="center" action="consultas/4.php" method="post">
+    O seleccine a uno:
+    <select name="nombre">
+      <?php
+      #Para cada tipo agregamos el tag <option value=value_of_param> visible_value </option>
+      foreach ($dataCollected as $d) {
+        echo "<option value=$d[0]>$d[0]</option>";
+      }
+      ?>
+    </select>
+    <br><br>
+    <input type="submit" value="Buscar">
+  </form>
 
   <br>
   <br>

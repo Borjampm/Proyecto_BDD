b8af390f0cd823cf192b209a0fb3d66aa12a3e9b
Borjampm <bmarquezdelaplata@uc.cl>
2022-11-06 17:06:09 -0300

    consulta 7

---
+++ consultas/7.php
@@ -0,0 +1,32 @@
+<?php include('../templates/header.html');   ?>
+
+<body>
+
+<?php
+  #Llama a conexión, crea el objeto PDO y obtiene la variable $db
+  require("../config/conexion.php");
+
+
+ 	$query = "SELECT artistas.nombre_artistico
+            FROM rntradas_cortesia INNER JOIN artistas ON entradas_cortesia.id_artista = artistas.id_artista
+            GROUP BY artistas.nombre_artistico
+            ORDER BY COUNT(artistas.nombre_artistico) DESC
+            LIMIT 1;";
+	$result = $db -> prepare($query);
+	$result -> execute();
+	$artistas = $result -> fetchAll();
+  ?>
+
+	<table align="center">
+    <tr>
+      <th>Nombre Artistico</th>
+      <th>Numero Contacto</th>
+    </tr>
+  <?php
+	foreach ($artistas as $artista) {
+  		echo "<tr> <td>$artista[0]</td></tr>";
+	}
+  ?>
+	</table>
+
+<?php include('../templates/footer.html'); ?>
--- index.php
+++ index.php
@@ -130,35 +130,5 @@
   <br>
   <br>
 
-
-
-  <h3 align="center">¿Quieres buscar todos los pokemones por tipo?</h3>
-
-  <?php
-  #Primero obtenemos todos los tipos de pokemones
-  require("config/conexion.php");
-  $result = $db -> prepare("SELECT DISTINCT tipo FROM pokemones;");
-  $result -> execute();
-  $dataCollected = $result -> fetchAll();
-  ?>
-
-  <form align="center" action="consultas/consulta_tipo.php" method="post">
-    Seleccinar un tipo:
-    <select name="tipo">
-      <?php
-      #Para cada tipo agregamos el tag <option value=value_of_param> visible_value </option>
-      foreach ($dataCollected as $d) {
-        echo "<option value=$d[0]>$d[0]</option>";
-      }
-      ?>
-    </select>
-    <br><br>
-    <input type="submit" value="Buscar por tipo">
-  </form>
-
-  <br>
-  <br>
-  <br>
-  <br>
 </body>
 </html>

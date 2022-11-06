b6d2612ca4a2c40b1e8504032cecec9168d35086
Borjampm <bmarquezdelaplata@uc.cl>
2022-11-06 16:18:55 -0300

    sejgqeuv

--- consultas/consulta_stats.php
+++ consultas/consulta_stats.php
@@ -9,7 +9,6 @@
   echo $nombre;
  	#$query = "SELECT * FROM pokemones where id = $id_nuevo;";
   $query = "SELECT artistas.nombre_artistico, COUNT(artistas.nombre_artistico) as entradas_de_cortesía_entregadas FROM entradas_cortesia INNER JOIN artistas ON entradas_cortesia.id_artista = artistas.id_artista WHERE LOWER(artistas.nombre_artistico) = LOWER($nombre) GROUP BY artistas.nombre_artistico;";
-  $query = "SELECT artistas.nombre_artistico, COUNT(artistas.nombre_artistico) as entradas_de_cortesía_entregadas FROM entradas_cortesia INNER JOIN artistas ON entradas_cortesia.id_artista = artistas.id_artista WHERE LOWER(artistas.nombre_artistico) = LOWER('Drake') GROUP BY artistas.nombre_artistico;";
 	$result = $db -> prepare($query);
 	$result -> execute();
 	$artistas = $result -> fetchAll();

2ee51279fc27f85408ee867933c3c28d5200b43a
Borjampm <bmarquezdelaplata@uc.cl>
2022-11-09 14:58:37 -0300

    wtf

--- consultas/2.php
+++ consultas/2.php
@@ -6,9 +6,9 @@
   require("../config/conexion.php");
 
   $nombre = $_POST["nombre"];
-  $query = "SELECT artistas.nombre_artistico, COUNT(artistas.nombre_artistico) as entradas_de_cortesía_entregadas 
+  $query = "SELECT artistas.nombre_artistico, COUNT(artistas.nombre_artistico) as entradas_de_cortesía_entregadas
             FROM entradas_cortesia INNER JOIN artistas ON entradas_cortesia.id_artista = artistas.id_artista
-            WHERE LOWER(artistas.nombre_artistico) = LOWER('Dj Snake')
+            WHERE LOWER(artistas.nombre_artistico) = LOWER('$nombre')
             GROUP BY artistas.nombre_artistico;";
 	$result = $db -> prepare($query);
 	$result -> execute();

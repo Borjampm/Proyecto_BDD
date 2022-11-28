c5a3b5afbebde8d3a5ad20628faa25bc0359a3b7
Borjampm <bmarquezdelaplata@uc.cl>
2022-11-28 17:48:07 -0300

    asd

--- views/login.php
+++ views/login.php
@@ -9,7 +9,7 @@
     <h3> Ingrese nombre de usuario y contraseña </h3>
     <br>
     <form class="form-signin" role="form" action="login_validation.php" method="post">
-        <?php echo $msg; ?>
+        <!-- <?php echo $msg; ?> -->
         <input type="text" name="username" placeholder="nombre de usuario" required autofocus>
         <input type="password" name="password" placeholder="contraseña" required>
         <button type="submit" name="login"> Iniciar sesión </button>

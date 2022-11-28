e7f826bce21c9b3ec8d3161e0b0dfbf457d45f9d
Borjampm <bmarquezdelaplata@uc.cl>
2022-11-28 16:54:35 -0300

    asf

--- queries/importar_usuarios.php
+++ queries/importar_usuarios.php
@@ -47,7 +47,7 @@
         // Ejecutamos las querys para efectivamente insertar los datos
         $result = $db1 -> prepare($query);
         $result -> execute();
-        $resultado_artista = $result -> fetchAll();
+        $result -> fetchAll();
     }
 
 

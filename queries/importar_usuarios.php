7e1f85ad50119b919fabeb0af0929722e9e73bc9
vidallucas <lucas.vidal@uc.cl>
2022-11-29 15:20:22 -0300

    asd

--- queries/importar_usuarios.php
+++ queries/importar_usuarios.php
@@ -48,9 +48,10 @@
         $result = $db1 -> prepare($query);
         $result -> execute();
         $resultado_artista = $result -> fetchAll();
+        print_r($resultado_artista)."/n";
         echo "STR";
         echo $resultado_artista[0]["importar_usuario"];
-        echo "STR\n";
+        echo "STR";
     }
 
 

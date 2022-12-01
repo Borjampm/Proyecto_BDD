c6f17621a542db5e0c36c5db83027ae1561c23bd
vidallucas <lucas.vidal@uc.cl>
2022-12-01 18:30:16 -0300

    asf

--- stored_procedures/crear_evento.sql
+++ stored_procedures/crear_evento.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE FUNCTION
 
 -- declaramos la función y sus argumentos
-crear_evento (nombre varchar(100), fecha_inicio date, ciudad varchar(30), pais varchar(30), recinto varchar(100), id_productora int, estado varchar(30))
+crear_evento (nombre varchar(100), fecha_inicio date, ciudad varchar(30), pais varchar(30), recinto_nombre varchar(100), id_productora int, estado varchar(30))
 
 -- declaramos lo que retorna, en este caso un booleano
 RETURNS INTEGER as $$
@@ -11,7 +11,7 @@ RETURNS INTEGER as $$
 -- definimos nuestra función
 BEGIN
 
-    IF fecha_inicio NOT IN (SELECT eventos.fecha_inicio, eventos.recinto from eventos WHERE eventos.recinto = recinto) THEN
+    IF fecha_inicio NOT IN (SELECT eventos.fecha_inicio from eventos WHERE eventos.recinto = recinto_nombre) THEN
         RETURN 1;
         --INSERT INTO eventos values(nombre, recinto, ciudad, pais, fecha_inicio, id_productora, estado);
 

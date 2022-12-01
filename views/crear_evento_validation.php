56197e0da9bff5b92882fc6b38b04b1ad6838683
vidallucas <lucas.vidal@uc.cl>
2022-12-01 18:41:29 -0300

    asdfaf

--- views/crear_evento_validation.php
+++ views/crear_evento_validation.php
@@ -25,7 +25,7 @@
         $resultado_evento = $resultado_evento[0]["crear_evento"];
         if ($resultado_evento == 0){
             $msg = "NO SE PUDO CREAR EL EVENTO";
-            header("Location: ./crear_evento.php?msg=$query");
+            header("Location: ./crear_evento.php?msg=$msg");
 
         } else{
             $msg = "EVENTO CREADO CORRECTAMENTE";

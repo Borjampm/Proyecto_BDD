406904785cd5763bd6b3d651d779fb17083c65e4
Borjampm <bmarquezdelaplata@uc.cl>
2022-12-01 22:43:48 -0300

    asdf

--- queries/importar_usuarios.php
+++ queries/importar_usuarios.php
@@ -75,7 +75,7 @@
     $usuarios = $result -> fetchAll();
 
 ?>
-    <form  action='../index.php' method='get'>
+    <form  action='~grupo13/index.php' method='get'>
         <input class='btn' type='submit' value='Volver'>
     </form>
 

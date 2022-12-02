98a66a0c19bc2f8bd396e7b14e437ba591ff79d4
Borjampm <bmarquezdelaplata@uc.cl>
2022-12-02 11:36:13 -0300

    asdf

--- views/entradas_cortesia.php
+++ views/entradas_cortesia.php
@@ -4,7 +4,7 @@ require("../config/conexion.php");
 
 $msg = $_GET['msg'];
 
-
+echo $msg;
 $query = "SELECT categoria, COUNT(categoria) as Numero_de_entradas FROM entradas WHERE nombre_evento = '$msg' GROUP BY categoria;";
 $result = $db2 -> prepare($query);
 $result -> execute();

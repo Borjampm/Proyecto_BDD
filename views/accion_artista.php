7f06e5cb6f42e5ac5b45b9463b43a514ed0a00d5
Borjampm <bmarquezdelaplata@uc.cl>
2022-12-02 00:28:19 -0300

    jghv

--- views/accion_artista.php
+++ views/accion_artista.php
@@ -2,7 +2,7 @@
 include('../templates/header.html');
 require("../config/conexion.php");
 
-$msg = $_GET['msg'];
+$msg = json_decode($_GET['msg']);
 $id_artista = $msg[0];
 $id_evento = $msg[1];
 $estado = $msg[3];

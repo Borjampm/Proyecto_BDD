8d48d032ef1108965b2f903771e0e05bf11395d8
Borjampm <bmarquezdelaplata@uc.cl>
2022-12-02 12:22:32 -0300

    asdc

--- views/home.php
+++ views/home.php
@@ -147,21 +147,10 @@ require("../config/conexion.php");
             <p>
                 Buscar eventos por fecha
             </p>
+            
             <form class="form-signin" role="form" action="home.php" method="post">
-            <?php
-            if (isset($_POST['desde']) && !empty($_POST['hasta']))
-                {
-                    $desde = $_POST['desde'];
-                    $hasta = $_POST['hasta'];
-                    echo '
-                    <input class="form-control" type="date" name="desde" value="$desde" required>
-                    <input class="form-control" type="date" name="hasta" value="$hasta" required>';
-                } else {
-                    echo '
-                    <input class="form-control" type="date" name="desde" value="2015-01-01" required>
-                    <input class="form-control" type="date" name="hasta" value="2022-12-31" required>';
-                }
-                ?>
+            <input class="form-control" type="date" name="desde" value="2015-01-01" required>
+            <input class="form-control" type="date" name="hasta" value="2022-12-31" required>
             <br>
             <button class='btn btn-outline-success' type="submit" name="home"> Filtrar </button>
         </div>

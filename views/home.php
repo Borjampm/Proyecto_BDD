804505fb24e9a41731c07530d9aff588a67dac4a
Martin Caldentey L <martincaldenteyl@MacBook-Air-de-Martin.local>
2022-12-02 13:59:44 -0300

    styles

--- views/home.php
+++ views/home.php
@@ -28,7 +28,7 @@ require("../config/conexion.php");
 
                 ?>
                                 <form align="center" action="./logout.php" method="post">
-                    <input lass='btn btn-outline-success' type="submit" value="Cerrar sesión">
+                    <input class='btn btn-outline-success' type="submit" value="Cerrar sesión">
                 </form>
                     <h2> Eventos Programados </h2>
                     <?php

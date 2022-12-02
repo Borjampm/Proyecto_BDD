3adac8850310079dd3a8432e7d35b4a85c0786be
Borjampm <bmarquezdelaplata@uc.cl>
2022-12-02 00:32:04 -0300

    asdf

--- views/home.php
+++ views/home.php
@@ -100,8 +100,7 @@ require("../config/conexion.php");
                                 echo "<td>$evento[6]</td> ";
                                 echo "<td>$evento[7]</td> ";
                                 echo "<td>$evento[8]</td> ";
-                                // $msg = json_encode(array($tipo_id, $evento[0], $evento[6], "Aprobado"));
-                                $msg = 10;
+                                $msg = json_encode(array($tipo_id, $evento[0], $evento[6], "Aprobado"));
                                 $msg2 = json_encode(array($tipo_id, $evento[0], $evento[6], "Rechazado"));
                                 echo '<td>
                                         <form align="center" action="./accion_artista.php?msg='.$msg.'" method="post">

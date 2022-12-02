0f61dbed2825ffc506ec4d717c908e02ac422663
Borjampm <bmarquezdelaplata@uc.cl>
2022-12-02 11:41:51 -0300

    asdf

--- views/entradas_cortesia.php
+++ views/entradas_cortesia.php
@@ -4,7 +4,6 @@ require("../config/conexion.php");
 
 $msg = $_GET['msg'];
 
-echo $msg;
 $query = "SELECT categoria, COUNT(categoria) as Numero_de_entradas FROM entradas WHERE nombre_evento = '$msg' GROUP BY categoria;";
 $result = $db2 -> prepare($query);
 $result -> execute();
@@ -33,5 +32,8 @@ if(empty($entradas)){
                 </table>
 
 <?php }?>
+<form align="center" action="./home.php" method="get">
+            <input type="submit" value="Volver a home">
+</form>
 
 

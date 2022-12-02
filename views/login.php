9b9c08695c5f172c3cf7df782de5c8d68305c5e9
Cristóbal Cuneo <cris@Cristobals-MacBook-Air.local>
2022-12-02 02:18:26 -0300

    lol

--- views/login.php
+++ views/login.php
@@ -10,12 +10,12 @@
 <?php include('../templates/header.html'); ?>
 
 <body>
-    <h3> Ingrese nombre de usuario y contraseña </h3>
+    <!-- <h3> Ingrese nombre de usuario y contraseña </h3> -->
     <br>
     <form class="form-signin" role="form" action="login_validation.php" method="post">
         <?php echo $msg; ?>
-        <input type="text" name="username" placeholder="nombre de usuario" required autofocus>
-        <input type="password" name="password" placeholder="contraseña" required>
+        <input class="form-control" type="text" name="username" placeholder="nombre de usuario" required autofocus>
+        <input class="form-control" type="password" name="password" placeholder="contraseña" required>
         <!-- <select type="tipo" name="tipo" placeholder="contraseña" required> -->
         <button type="submit" name="login"> Iniciar sesión </button>
     </form>

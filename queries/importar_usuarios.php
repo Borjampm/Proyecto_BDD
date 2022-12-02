0386439d0644cdf6568f8d2a35b4e20738eaa767
Cristóbal Cuneo <cris@Cristobals-MacBook-Air.local>
2022-12-02 02:08:39 -0300

    lol

--- queries/importar_usuarios.php
+++ queries/importar_usuarios.php
@@ -75,6 +75,7 @@
     $usuarios = $result -> fetchAll();
 
 ?>
+<br>
     <form action='../index.php' align="center" method='get'>
         <input class='btn btn-outline-success' type='submit' value='Volver'>
     </form>
